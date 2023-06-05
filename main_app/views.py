990eb0de54179c7ce878d1184339377b18b7b670
bbonning4 <bbonning42@gmail.com>
2023-06-05 14:20:42 -0500

    add search

--- main_app/views.py
+++ main_app/views.py
@@ -6,10 +6,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Profile
+import requests
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    error_msg = ""
+    return render(request, 'home.html', {'error_msg':error_msg})
 
 def signup(request):
     error_message = ''
@@ -29,3 +31,16 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)  
+
+def search(request):
+    pokemon_name = request.GET.get('pokemon_name')
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        json = response.json()
+        name = json["name"].capitalize()
+        image = json["sprites"]["other"]["official-artwork"]["front_default"]
+    else:
+        error_msg = "No Results"
+        return render(request, 'home.html', {"error_msg": error_msg})
+    return render(request, 'pokemon/detail.html', {'name': name, 'image': image})    
