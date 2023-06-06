68fd1159bd6cacd5ba1fd024fb1eb78b41e3c338
btb0 <benburke2016@gmail.com>
2023-06-06 13:42:26 -0400

    updates Favorite/Profile model

--- main_app/models.py
+++ main_app/models.py
@@ -5,15 +5,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.CharField(default='https://w0.peakpx.com/wallpaper/416/975/HD-wallpaper-slowpoke-pokemon-face-eyes.jpg')
+    avatar = models.CharField(max_length=250, default='https://w0.peakpx.com/wallpaper/416/975/HD-wallpaper-slowpoke-pokemon-face-eyes.jpg')
 
     def __str__(self):
         return self.user.username
 
 class Favorite(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    name = models.CharField()
-    image = models.CharField()
+    name = models.CharField(max_length=50)
+    image = models.CharField(max_length=250)
     is_shiny = models.BooleanField(default=False)
 
     def __str__(self):
